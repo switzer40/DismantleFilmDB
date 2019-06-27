0333059a5bb0bff62592b7a050abeec8a0e5d834
Robert Switzer <switzer40@googlemail.com>
2019-06-28 01:44:33 +0200

    Just getting started

---
+++ Dismantle/Dismantle/ExtractPeople.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dismantle
+{
+    public class ExtractPeople
+    {
+    }
+}
---
+++ Dismantle/Dismantle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dismantle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
